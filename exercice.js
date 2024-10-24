229c6f616ee1060ff52902f449b605eaf45b7478
achrefmechergui <achreffcb123achref@gamil.com>
2024-10-24 20:09:53 +0100

    update/24/10

---
+++ exercice.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+//1// //////// Crée un tableau vide et utilise la méthode ***** 
+// pour y ajouter les nombres de 1 à 5. Affiche le tableau avec console.log.
+
+
+// ///2// Crée un tableau avec les nombres [1, 2, 3, 4, 5]. 
+// Utilise la méthode ***** pour retirer le dernier élément et affiche le tableau mis à jour.
+
+
+
+// ///3// Crée un tableau avec les nombres [3, 4, 5]. 
+// Utilise la méthode **** pour ajouter les nombres 1 et 2 au début du tableau.
+
+
+// //// //4// Crée un tableau avec les nombres [1, 2, 3, 4, 5]. 
+// Utilise la méthode ***** pour retirer le premier élément et affiche le tableau mis à jour.
+
+
+
+// //// 5///  Crée un tableau avec les nombres [10, 20, 30, 40, 50].
+//  Utilise la méthode **** pour trouver l'index de l'élément 30 et affiche-le.
+
+
+// /// 6 /// Crée un tableau avec les nombres [100, 200, 300, 400, 500]. 
+// Utilise la propriété ******* pour afficher la taille du tableau.
+
+// ///// 7 // Crée un objet vide et ajoute une key(clé) name 
+// avec la valeur (value) "Alice". Affiche l'objet dans la console.
+
+
+//// 8 /// Crée un objet vide et ajoute les paires clé-valeur suivantes : age : 25, city : "Paris". Affiche l'objet.
+
+/// 9// Crée un objet avec les clés name : "Bob" et age : 30. Utilise console.log pour afficher la valeur de la clé name.
+
+
+// ////10 ///  Crée un tableau avec les nombres [10, 20, 30] et un objet vide. 
+// Retire le dernier élément du tableau avec pop et ajoute-le comme valeur d'une clé lastNumber dans l'objet. 
+// Affiche l'objet.
